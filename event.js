3333edc7eccba504a169e5ee98c615c0a030ab97
Sajeed Ansari <102681838+SajeedAns786@users.noreply.github.com>
2023-10-24 20:50:36 +0530

    event

---
+++ event.js
@@ -0,0 +1,5 @@
+//Create and trigger custom event in node js
+const events = require("events");
+const event = new events.EventEmitter();
+event.on("click", (n1, n2) => console.log(n1 + n2));
+event.emit("click", "Hello Sajeed ", "Ansari");
